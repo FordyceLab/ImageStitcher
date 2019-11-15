29b445faf41cb016f3d461c59f2a5aeb2eaf58c0
Mokhaccino <mokhda@stanford.edu>
2019-11-15 00:44:55 -0800

    Added support for MicroManager Stitching
    Can now stitch .ome.tiff stacks from Micromanager, with updated notebook and example

--- imagestitcher/stitcher.py
+++ imagestitcher/stitcher.py
@@ -3,7 +3,7 @@
 # authors           : Daniel Mokhtari
 # credits           : Craig Markin, Polly Fordyce
 # date              : 20180520
-# version update    : 20191001
+# version update    : 20191115
 # version           : 0.1.0
 # usage             : With permission from DM
 # python_version    : 3.7
@@ -24,6 +24,7 @@ import numpy as np
 import pandas as pd
 from matplotlib import pyplot as pl
 from skimage import io, transform, external
+from PIL import Image, ImageSequence
 
 
 
@@ -314,6 +315,69 @@ class FileHandler:
 
 
 
+
+
+class MMFileHandler:
+
+    @staticmethod
+    def readMMMetaData(path):
+        fh = external.tifffile.tifffile.FileHandle(path, mode='rb', name=None, offset=None, size=None)
+        md = external.tifffile.tifffile.read_micromanager_metadata(fh)
+        fh.close()
+        return md
+
+
+    @staticmethod
+    def mdToDF(micromanager_metadata_dict, p):
+        md = micromanager_metadata_dict
+        summary = md['summary']
+        channels = summary['ChNames']
+        ordered_positions = [(p['GridColumnIndex'], p['GridRowIndex']) for p in MMFileHandler.readMMMetaData(p)['summary']['InitialPositionList']]
+
+        # Does the key "Inteval_ms" exist?
+        if 'Interval_ms' in summary.keys():
+            fixedIntervalTimes = summary['Interval_ms']
+        else:
+            fixedIntervalTimes = None
+
+        frames = summary['Frames']
+        customTimes_s = [t/1000. for t in summary['CustomIntervals_ms']]
+        delay_times = np.cumsum([t/1000. for t in summary['CustomIntervals_ms']])
+        dims = (summary['Width'], summary['Height'])
+        channel_index = md['index_map']['channel']
+        position = md['index_map']['position'][0]
+
+
+        baseRecord = {'num_frames_total': frames, 'dims': dims,'position': position, 'x': ordered_positions[position][0], 'y': ordered_positions[position][1]}
+
+        records = []
+        for f in range(frames):
+            for c in range(len(channels)):
+                sliceRecord = {'stack_index': f*len(channels)+c, 'channel': channels[c], 'time_interval': customTimes_s[f],'delay_time': delay_times[f]}
+                records.append({**sliceRecord, **baseRecord})
+
+        metadata_df = pd.DataFrame(records)
+        return metadata_df
+
+
+    @staticmethod
+    def parseStackMD(path):
+        md = MMFileHandler.readMMMetaData(path)
+        md_df = MMFileHandler.mdToDF(md, path)
+        md_df['path'] = str(path)
+        return md_df
+
+
+    @staticmethod
+    def parseMMStackedFolder(root, channelExposureMap, remapChannels = None):
+        metadata = pd.concat([MMFileHandler.parseStackMD(str(folder)) for folder in pathlib.Path(str(root)).iterdir() if 'tif' in folder.suffix])
+        metadata['exp'] = metadata.channel.apply(lambda c: channelExposureMap[c])
+        if remapChannels:
+            metadata['channel'] = metadata.channel.apply(lambda c: remapChannels[c])
+        return metadata
+
+
+
 class Raster:
     def __init__(self, imageRefs, params):
         """
@@ -406,6 +470,7 @@ class Raster:
 
         trimmedTiles = [tile[border, border] for tile in tiles] #Trim
         tileArray = np.asarray(trimmedTiles) #Make ndarray
+        
         arrangedTiles = np.reshape(tileArray, (self.params.dims[0], self.params.dims[1], retained, retained))
         if self.params.acquiOri[0]: #If origin on right, flip horizontally (view returned)
             arrangedTiles = np.flip(arrangedTiles, 0)
@@ -483,7 +548,7 @@ class FlatRaster(Raster):
 
 
 class StackedRaster(Raster):
-    def __init__(self, imageRefs, stackIndices, params):
+    def __init__(self, imageRefs, stackIndex, params):
         """
 
         Arguments:
@@ -492,8 +557,8 @@ class StackedRaster(Raster):
             None
 
         """
-        super().__init__(imageRefs, stackIndices)
-        self.stackIndices = stackIndices
+        super().__init__(imageRefs, params)
+        self.stackIndex = stackIndex
 
 
     def fetchImages(self):
@@ -507,7 +572,23 @@ class StackedRaster(Raster):
             None
 
         """
-        return
+        r = self.params.rotation
+    
+        rotationParams = {'resize': False, 'clip': True, 'preserve_range': True}
+        rotateImage = lambda i: transform.rotate(i, r, **rotationParams).astype('uint16')
+        
+        def readImage(path):
+            with Image.open(path) as img:
+                data = np.asarray(ImageSequence.Iterator(img)[self.stackIndex])
+            return data
+            # readImage = lambda i: ImageSequence.Iterator()[self.stackIndex]
+
+        images = [readImage(i) for i in self.imageRefs]
+        
+        if r:
+            images = [rotateImage(i) for i in images]
+        
+        self.images = images
 
 
 
@@ -813,4 +894,52 @@ def walkAndStitch(path, params, stitchtype = 'kinetic'):
                     stitchImage(target, newParams)
             else:
                 raise ValueError('Valid Stich Types are "single" or "kinetic"')
+
+
+
+def MMStitchStacks(root, overlap, setup, channelExposureMap, autoFF = False, channelRemap = None):
+    raster_metadata = MMFileHandler.parseMMStackedFolder(root, channelExposureMap, remapChannels = channelRemap)
+    
+    channels = raster_metadata.channel.unique().tolist()
+    exposures = raster_metadata.exp.unique().tolist()
+    delay_times = sorted(raster_metadata.delay_time.unique().tolist())
+    size = raster_metadata.dims.unique().tolist()[0][0]
+    dims = (max(raster_metadata.x)+1, max(raster_metadata.y)+1)
+
+    params = RasterParams(root, None, overlap, None, None, setup, autoFF = autoFF)
+    params.dims = dims
+    params.size = int(size)
+
+    rasters = []
+    for channel in channels:
+            # stitch a set of timepoints
+            for time in tqdm(delay_times, desc = 'Stitching Kinetics | {} | {}'.format(channel, pathlib.Path(root).stem)):
+                if channelRemap:
+                    invertedChannelRemap = dict(zip(channelRemap.values(), channelRemap.keys()))
+                    exposure = channelExposureMap[invertedChannelRemap[channel]]
+                else:
+                    exposure = channelExposureMap[channel]
+                toStitch = raster_metadata.loc[(raster_metadata.channel == channel) 
+                                               & (raster_metadata.exp == exposure)
+                                                & (raster_metadata.delay_time == time)
+                                              ].sort_values(['x', 'y']).reset_index(drop = True).set_index('exp')
+                paths = toStitch.path.values.tolist()
+                stackIndex = toStitch.stack_index.unique()[0]
                 
+                newparams = deepcopy(params)
+                newparams.exposure = exposure
+                newparams.channel = channel
+                newparams.groupFeature = time
+                
+                stacked_raster = StackedRaster(paths, stackIndex, newparams)
+                stacked_raster.fetchImages()
+                
+                stitchedRaster = stacked_raster.stitch()
+                
+                features = [stacked_raster.params.exposure, stacked_raster.params.channel, int(time)]
+                rasterName = 'StitchedImg_{}_{}_{}.tif'.format(*features)
+                
+                stitchDir = pathlib.Path(os.path.join(params.root, pathlib.Path('StitchedImages')))
+                stitchDir.mkdir(exist_ok = True)
+                outDir = os.path.join(stitchDir, rasterName)
+                external.tifffile.imsave(outDir, stitchedRaster);
